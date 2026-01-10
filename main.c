97f411a99bc5ab0951f90e00da7711eda641c91a
Flunkie S. <flunkademic@gmail.com>
2026-01-10 17:18:32 +0000

    Add files via upload

--- main.c
+++ main.c
@@ -6,10 +6,11 @@
 /*   By: laraus <laraus@student.lisboa42.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 18:06:30 by laraus            #+#    #+#             */
-/*   Updated: 2026/01/08 16:43:14 by laraus           ###   ########.fr       */
+/*   Updated: 2026/01/10 15:20:07 by laraus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
 #include "get_next_line.h"
 #include <stdio.h>
 #include <fcntl.h>
@@ -97,4 +98,56 @@ int main(void)
 
     return (0);
 }
+*/
 
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	char *files[] = {
+		"files/empty",
+		"files/nl",
+		"files/41_no_nl",
+		"files/41_with_nl",
+		"files/42_no_nl",
+		"files/42_with_nl",
+		"files/43_no_nl",
+		"files/43_with_nl",
+		"files/multiple_nlx5",
+		"files/multiple_line_no_nl",
+		"files/multiple_line_with_nl",
+		"files/alternate_line_nl_no_nl",
+		"files/alternate_line_nl_with_nl",
+		"files/big_line_no_nl",
+		"files/big_line_with_nl"
+	};
+
+	int num_files = sizeof(files) / sizeof(files[0]);
+	for (int i = 0; i < num_files; i++)
+	{
+		int fd = open(files[i], O_RDONLY);
+		if (fd < 0)
+		{
+			perror(files[i]);
+			continue;
+		}
+
+		printf("\n=== Testing file: %s ===\n", files[i]);
+		char *line;
+		int line_num = 1;  // track line numbers for clarity
+		while ((line = get_next_line(fd)) != NULL)
+		{
+			// Print line number, file, and brackets
+			printf("%2d | [%s]\n", line_num, line);
+			free(line);
+			line_num++;
+		}
+		close(fd);
+		printf("=== End of %s ===\n", files[i]);
+	}
+
+	return 0;
+}
